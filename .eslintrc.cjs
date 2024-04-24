e74ff8b9ea96c32f9d4eb2c0bda20f90ca9cdc65
Luiz Filipe da Silva de Jesus <suvilao@gmail.com>
2024-04-24 20:29:09 -0300

    configure prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,16 +3,20 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    'eslint-config-prettier',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  settings: { react: { version: '18.2' } },
+  plugins: ['react-refresh', 'import'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'import/newline-after-import': ['error', { count: 1 }],
   },
-}
+};
